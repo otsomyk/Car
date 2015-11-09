90da8184042306d73d012be84b8afa047d274706
otsomyk <otsomyk@users.noreply.github.com>
2015-11-09 15:55:44 +0200

    Create README.md

---
+++ PrintCarInfo.java
@@ -0,0 +1,11 @@
+package car;
+
+public class PrintCarInfo {
+
+	public static void main(String[] args) {
+		Car car = new Car("Renault Duster", Colour.WHITE, 2012, 1.6);
+		car.printInfo();
+		
+	}
+	
+}
